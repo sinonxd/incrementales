dda9f6bf346dc6a18a84937d9e3a2f1709c2cb6d
sinonxd <juaneskfire@gmail.com>
2025-07-10 11:33:28 -0500

    Primer commit del proyecto tooltip-footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TooltipComponent } from './tooltip/tooltip.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    TooltipComponent,
+    FooterComponent,
+    HomeComponent 
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tienda-tooltip-footer';
-}
+export class AppComponent {}
---
+++ src/app/tooltip/tooltip.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tooltip',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tooltip.component.html',
+  styleUrls: ['./tooltip.component.css']
+})
+export class TooltipComponent {
+  mostrarMenu: boolean = false;
+
+  toggleMenu() {
+    this.mostrarMenu = !this.mostrarMenu;
+  }
+}
+
+
